1e42df26dd27ff336739c2d106c1726458dada3a
mamun4447 <abdullah15-12557@diu.edu.bd>
2023-02-28 00:21:48 +0600

    almost done

--- src/components/Photos.jsx
+++ src/components/Photos.jsx
@@ -26,7 +26,6 @@ const Photos = ({ photos, setPhotos, search }) => {
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:mx-auto mx-4  py-5 gap-5 justify-center items-center dark:bg-zinc-800 px-20">
       {photos.map((photo) => (
         <SingleCard
-          search={search}
           key={photo.id}
           setSingleInfo={setSingleInfo}
           photo={photo}
